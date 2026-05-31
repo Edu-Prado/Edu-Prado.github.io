86f95a975b6d04eae9d969f5451506bcbba36ae3
Edu-Prado <epradojunior@hotmail.com>
2026-05-31 17:50:50 -0300

    feat(analytics): add Google Analytics tag to _app.js

--- next-site/pages/_app.js
+++ next-site/pages/_app.js
@@ -1,5 +1,28 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-QYZ06TPTGJ"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-QYZ06TPTGJ');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
